62ac14f0878f78014f5f60acb273c6465e72ae9e
rhodeon <ruonabraks@gmail.com>
2020-12-25 00:18:39 +0100

    Touched up recycler view for posts.

--- app/src/main/java/com/rhodeon/habitforreddit/ui/home/HomeFeedFragment.kt
+++ app/src/main/java/com/rhodeon/habitforreddit/ui/home/HomeFeedFragment.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.rhodeon.habitforreddit.databinding.FragmentHomeFeedBinding
 import com.rhodeon.habitforreddit.models.link.LinkListing
 
@@ -38,6 +39,7 @@ class HomeFeedFragment : Fragment() {
 
 
         homeViewModelFactory = HomeViewModelFactory(token)
+        setHasOptionsMenu(true)
 
         _binding = FragmentHomeFeedBinding.inflate(inflater, container, false)
         return binding.root
@@ -48,6 +50,7 @@ class HomeFeedFragment : Fragment() {
 
         val adapter = HomeListAdapter()
         binding.postRecyclerView.adapter = adapter
+        binding.postRecyclerView.addItemDecoration(DividerItemDecoration(this.requireContext(), DividerItemDecoration.VERTICAL))
 
         val viewModelObserver = Observer<LinkListing> { response ->
             adapter.submitList(response.data.children)
