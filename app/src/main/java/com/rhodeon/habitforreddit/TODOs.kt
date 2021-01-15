475a5aec106a62810e61f45718b2d0fd2b839474
rhodeon <ruonabraks@gmail.com>
2021-01-15 02:59:29 +0100

    Added a TODO file.

---
+++ app/src/main/java/com/rhodeon/habitforreddit/TODOs.kt
@@ -0,0 +1,5 @@
+package com.rhodeon.habitforreddit
+
+// TODO 1: Further comments implementation (depth indicator, more replies, etc).
+// TODO 2: Use Hilt to inject retrofit, moshi and markwon instances.
+// TODO 3: General response handling (prevent crashes on request errors)
