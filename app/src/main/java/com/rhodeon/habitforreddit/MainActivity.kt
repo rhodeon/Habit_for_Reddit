fa4524432314ce41ff55305fb66c995abc0e9779
rhodeon <ruonabraks@gmail.com>
2021-01-20 02:11:54 +0100

    Hid the bottom nav in the thread fragment.

--- app/src/main/java/com/rhodeon/habitforreddit/MainActivity.kt
+++ app/src/main/java/com/rhodeon/habitforreddit/MainActivity.kt
@@ -6,6 +6,9 @@ import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.rhodeon.habitforreddit.databinding.ActivityMainBinding
+import com.rhodeon.habitforreddit.extensions.collapse
+import com.rhodeon.habitforreddit.extensions.hide
+import com.rhodeon.habitforreddit.extensions.show
 import com.rhodeon.habitforreddit.network.oauth.requestAccessToken
 import com.rhodeon.habitforreddit.utils.SessionManager
 import kotlinx.coroutines.*
@@ -30,6 +33,13 @@ class MainActivity : AppCompatActivity() {
             navGraph.startDestination = R.id.homeFeedFragment
             navController.graph = navGraph
             binding.bottomNavigationView.setupWithNavController(navController)
+
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.threadFragment -> binding.bottomNavigationView.collapse()
+                    else -> binding.bottomNavigationView.show()
+                }
+            }
         }
     }
 }
